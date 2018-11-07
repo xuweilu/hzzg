23ad7a082867c8471e66fd3f66f01c7dfcf9d4c6
william <xuweiluwx@outlook.com>
2018-11-07 23:11:35 +0800

    finish

--- src/mock/carouselList.js
+++ src/mock/carouselList.js
@@ -5,37 +5,48 @@ import s03 from "../assets/images/steels/IMG_7109.jpg";
 export default {
   dataList: [
     {
-      image: s01
+      image: s01,
+      des: "钢材仓库钢材仓库钢材仓库钢材仓库钢材仓库钢材仓库"
     },
     {
-      image: s02
+      image: s02,
+      des: "钢材仓库钢材仓库钢材仓库钢材仓库钢材仓库钢材仓库"
     },
     {
-      image: s03
+      image: s03,
+      des: "钢材仓库钢材仓库钢材仓库钢材仓库钢材仓库钢材仓库"
     },
     {
-      image: s01
+      image: s01,
+      des: "钢材仓库钢材仓库钢材仓库钢材仓库钢材仓库钢材仓库"
     },
     {
-      image: s02
+      image: s02,
+      des: "钢材仓库钢材仓库钢材仓库钢材仓库钢材仓库钢材仓库"
     },
     {
-      image: s03
+      image: s03,
+      des: "钢材仓库"
     },
     {
-      image: s01
+      image: s01,
+      des: "钢材仓库"
     },
     {
-      image: s02
+      image: s02,
+      des: "钢材仓库"
     },
     {
-      image: s03
+      image: s03,
+      des: "钢材仓库"
     },
     {
-      image: s01
+      image: s01,
+      des: "钢材仓库"
     },
     {
-      image: s02
+      image: s02,
+      des: "钢材仓库"
     }
   ]
 }
